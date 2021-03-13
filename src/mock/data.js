286193a1f1b607d75cbf8c5c46d0658819fb653c
Michael Eclavea <usermike@gmail.com>
2021-03-13 02:36:25 -0800

    test

--- src/mock/data.js
+++ src/mock/data.js
@@ -19,8 +19,10 @@ export const heroData = {
 export const aboutData = {
   img: 'profile.jpg',
   paragraphOne: "Hello, my name is Michael Eclavea. I'm a full stack Software Developer.",
-  paragraphTwo: 'I have completed a rigorous boot-camp at CodeFellows while simultaneously completing other certificates in the process.',
-  paragraphThree: "I have over a decade of experience working on computers, which includes repairing hardware, debugging software related issues and even building them from the ground up. I have also worked in the healthcare industry for the past 10 years. During this time, I've worked with dynamic teams consisting of diverse backgrounds, cultures and views. I work extremely well in team settings, but I'm also a strong independent worker. I am very self driven and love to let my work represent me.",
+  paragraphTwo:
+    'I have completed a rigorous boot-camp at CodeFellows while simultaneously completing other certificates in the process.',
+  paragraphThree:
+    "I have over a decade of experience working on computers, which includes repairing hardware, debugging software related issues and even building them from the ground up. I have also worked in the healthcare industry for the past 10 years. During this time, I've worked with dynamic teams consisting of diverse backgrounds, cultures and views. I work extremely well in team settings, but I'm also a strong independent worker. I am very self driven and love to let my work represent me.",
   resume: 'https://www.resumemaker.online/es.php', // if no resume, the button will not show up
 };
 
@@ -30,16 +32,18 @@ export const projectsData = [
     id: nanoid(),
     img: './videos/InspiredFitnessProject.mp4',
     title: 'Inspired Fitness & Training Facility',
-    info: 'This site was created for a clients fitness shop. An online e-commerce site made with React, Redux, MongoDB, JavaScript, HTML & CSS.',
+    info:
+      'This site was created for a clients fitness shop. An online e-commerce site made with React, Redux, MongoDB, JavaScript, HTML & CSS.',
     info2: '',
     url: 'https://inspiredfitness.herokuapp.com/',
     repo: 'https://github.com/MichaelEclavea/InspiredFitnessShop', // if no repo, the button will not show up
   },
   {
     id: nanoid(),
-    img: './videos/CodeHeroesProject.mp4',
+    img: './videos/CodeQuestProject.mp4',
     title: 'Code Heroes Quest',
-    info: 'A full story text-based web game. Code Fellows 401 JavaScript Final Project. Built using React, JavaScript, Socket.io, Express, chalk, jest, HTML, CSS, Inquirer and Inkarnate.',
+    info:
+      'A full story text-based web game. Code Fellows 401 JavaScript Final Project. Built using React, JavaScript, Socket.io, Express, chalk, jest, HTML, CSS, Inquirer and Inkarnate.',
     info2: '',
     url: 'https://codeheroesquest.herokuapp.com/',
     repo: 'https://github.com/code-heros-quest/codeHerosQuest', // if no repo, the button will not show up
@@ -48,7 +52,8 @@ export const projectsData = [
     id: nanoid(),
     img: './videos/MyQuickPickProject.mp4',
     title: 'My Quick Pick',
-    info: 'A application that is designed to allow people to better tailor their lotto quick picks to a more specified range. Built with React.js, JavaScript, HTML, CSS, Node.js and node-sass.',
+    info:
+      'A application that is designed to allow people to better tailor their lotto quick picks to a more specified range. Built with React.js, JavaScript, HTML, CSS, Node.js and node-sass.',
     info2: '',
     url: 'https://my-quick-pick.herokuapp.com/',
     repo: 'https://github.com/MichaelEclavea/QuickPick', // if no repo, the button will not show up
@@ -65,7 +70,6 @@ export const contactData = {
 // FOOTER DATA
 export const footerData = {
   networks: [
-
     {
       id: nanoid(),
       name: 'linkedin',
